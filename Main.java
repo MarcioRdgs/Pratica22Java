dc55fd9db570f226332eac77fa8b35c2ff509e57
MarcioR. <113216304+MarcioRdgs@users.noreply.github.com>
2024-10-05 01:43:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+package pratic_22;
+
+public class Data{
+  //atributos
+  private int dia, mes, ano;
+
+  //gettes e setters que são responsaveis pelo encapsulamento
+  public int getDia(){
+    return dia;
+  }
+  public void setDia(int dia){
+    this.dia = dia;
+  }
+  //gettes e setters que são responsaveis pelo encapsulamento
+  public int getMes(){
+    return mes;
+  }
+  public void setMes(int mes){
+    this.mes = mes;
+  }
+  //gettes e setters que são responsaveis pelo encapsulamento
+  public int getAno(){
+    return ano;
+  }
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+}
